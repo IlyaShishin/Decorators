a13f9d287690a7e6f2c3dc570dd86b65f5e7d8a5
Ilya Shishin <Shishinilya030996@mail.ru>
2021-06-01 19:21:13 +0400

    Second commit

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ def logger(log_path):
         def new_function(*args, **kwargs):
             name = f'Вызвана функция: {function.__name__}'
             params = f'С параметрами: {args}, {kwargs}'
-            result = function(*args, **kwargs)
+            result_calculate = function(*args, **kwargs)
             with open(log_path, 'a', encoding='utf-8') as f:
-                f.write(f'{datetime.now()}\n {name}\n {params}\n Полученный результат: {result}\n')
-            return f'Информация записана в файл'
+                f.write(f'{datetime.now()}\n {name}\n {params}\n Полученный результат: {result_calculate}\n')
+            return result_calculate
         return new_function
     return decorator
 
 
 @logger(r'C:\Users\Илья\PycharmProjects\Decorators\Log_result.txt')
 def calculate(a, b):
-    result = a + b
-    return result
+    result_calculate = a + b
+    return result_calculate
 
 
 print(calculate(2, 3))
